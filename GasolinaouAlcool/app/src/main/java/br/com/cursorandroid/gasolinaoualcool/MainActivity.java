86447294f398ef3def99fa90f2003a948b100f8c
jhowcs <jhowcs@gmail.com>
2016-01-09 17:11:15 -0200

    I fixed the bug in xml because both edtAlcool and edtGasolina just allowed integer. I also created the code in MainAcitivity.java to calculate and show to the user which is worth to put on the vehicle alchool or gas.

--- GasolinaouAlcool/app/src/main/java/br/com/cursorandroid/gasolinaoualcool/MainActivity.java
+++ GasolinaouAlcool/app/src/main/java/br/com/cursorandroid/gasolinaoualcool/MainActivity.java
@@ -2,12 +2,49 @@ package br.com.cursorandroid.gasolinaoualcool;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edtAlcool;
+    private EditText edtGasolina;
+    private Button btnVerificar;
+    private TextView txtResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtAlcool       = (EditText) findViewById(R.id.edtAlcool);
+        edtGasolina     = (EditText) findViewById(R.id.edtGasolina);
+        btnVerificar    = (Button) findViewById(R.id.btnVerificar);
+        txtResultado    = (TextView) findViewById(R.id.txtResultado);
+
+        btnVerificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final double resultado = getValorCalculo(Double.parseDouble(edtAlcool.getText().toString()), Double.parseDouble(edtGasolina.getText().toString()));
+
+                atualizarValorTela(resultado);
+            }
+        });
+    }
+
+    private void atualizarValorTela(double resultado) {
+        if(resultado >= 0.7) {
+            txtResultado.setText("É melhor utilizar Gasolina!");
+        } else {
+            txtResultado.setText("É melhor utilizar Álcool!");
+        }
+    }
+
+    private double getValorCalculo(double alcool, double gasolina) {
+        return alcool / gasolina;
     }
 }
